e0b82622eab2bcf2da28dc8655f095e27f8d4830
Дарья Головкина <@itstep.loc>
2023-08-28 20:48:26 +0600

    button

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup>
+import { ref } from "vue";
+const counter = ref(0);
+
+const increase = () => {
+  counter.value++
+}
+
+const decrease = () => {
+  counter.value--
+}
 </script>
 
 <template>
-<div>
-  РАБОТАЕТ
+<div class="test">
+  {{ counter }}
+  <button @click="increase">+</button>
+  <button @click="decrease">-</button>
 </div>
 </template>
 
